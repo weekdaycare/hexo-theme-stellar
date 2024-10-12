9f0129aefabbb716a238ee5b7f4d2c2f8ef21d44
weekdaycare <xiabee@duck.com>
2024-10-12 20:27:22 +0800

    Revert "[opt] function"
    This reverts commit f24f25ff444daf6384bb46d154e4cef6a2cb12d8.

--- source/js/services/mdrender.js
+++ source/js/services/mdrender.js
@@ -1,16 +1,14 @@
 utils.jq(() => {
-  $(function () {
-    const els = document.getElementsByClassName('ds-mdrender');
-    for (var i = 0; i < els.length; i++) {
-      const el = els[i];
-      const loadMarkdown = () => {
-        const src = `${el.getAttribute('src')}?t=${new Date().getTime()}`;
-        utils.request(el, src, function(data) {
-          el.innerHTML = marked.parse(resp.data);
-        });
-      };
-      const lazyload = el.hasAttribute('lazyload');
-      util.viewportLazyload(el, loadMarkdown, lazyload);
-    }
-  });
+  const els = document.getElementsByClassName('ds-mdrender');
+  for (var i = 0; i < els.length; i++) {
+    const el = els[i];
+    const loadMarkdown = () => {
+      const src = `${el.getAttribute('src')}?t=${new Date().getTime()}`;
+      utils.request(el, src, function(data) {
+        el.innerHTML = marked.parse(resp.data);
+      });
+    };
+    const lazyload = el.hasAttribute('lazyload');
+    util.viewportLazyload(el, loadMarkdown, lazyload);
+  }
 });
