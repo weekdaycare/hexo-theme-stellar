933621502e7a8a8c425e319ad936afeede72192e
weekdaycare <xiabee@duck.com>
2024-10-16 19:50:55 +0800

    [feat] homepage swiper - closed #5 - 基于本人现有相关 cover 进行魔改，在套用至stellar上时请请检查兼容性(尤其是post.cover)

--- scripts/filters/index.js
+++ scripts/filters/index.js
@@ -2,3 +2,9 @@
 
 hexo.extend.filter.register('after_render:html', require('./lib/img_lazyload').processSite);
 hexo.extend.filter.register('after_render:html', require('./lib/img_onerror').processSite);
+hexo.extend.filter.register('before_generate', () => {
+  const sortedPosts = hexo.locals.get('posts').toArray()
+    .filter(post => post.swiper_index >= 0)
+    .sort((a, b) => b.swiper_index - a.swiper_index);
+  hexo.locals.set('swiper_posts', sortedPosts);
+});
